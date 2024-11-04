efb3c51b47f4eb2db2159b0e0f9e33214199344a
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-04 10:54:28 +0700

    fix: Rescue

--- generate-cask.rb
+++ generate-cask.rb
@@ -10,4 +10,6 @@ tap.cask_files.each do |path|
   json = JSON.pretty_generate(cask.to_hash_with_variations)
 
   IO.write("_data/cask/#{name}.json", "#{json}\n")
+rescue
+  onoe "Error while generating data for '#{path.stem}'"
 end
