b98b4916b406b9cf5e0240d1d95eaaaf92e3baac
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-04 11:09:28 +0700

    docs: Reference links

--- generate-cask.rb
+++ generate-cask.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env brew ruby
+
+# Simplified version of `generate-cask-api` and `generate-cask`.
+#
+# REF: https://github.com/Homebrew/formulae.brew.sh/blob/998358b5d3d4e8710696e841d8fb195cebedd8dd/script/generate-cask.rb
+# REF: https://github.com/Homebrew/brew/blob/4d0deeb68e488fd2208c8a1058c506e70b182a63/Library/Homebrew/dev-cmd/generate-cask-api.rb#L47-L63
+
 require "cask/cask"
 
 tap_name = ARGV.first
 tap = Tap.fetch(tap_name)
+
+Cask::Cask.generating_hash!
 
 tap.cask_files.each do |path|
   cask = Cask::CaskLoader.load(path)
