4e372cb475b3835dd58870ab415551577e52327a
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-04 10:40:20 +0700

    fix: Create the dir first

--- generate-cask.rb
+++ generate-cask.rb
@@ -10,6 +10,4 @@ tap.cask_files.each do |path|
   json = JSON.pretty_generate(cask.to_hash_with_variations)
 
   IO.write("_data/cask/#{name}.json", "#{json}\n")
-rescue
-  onoe "Error while generating data for '#{path.stem}'"
 end
