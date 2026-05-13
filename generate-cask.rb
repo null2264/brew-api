f1056d32fa20135c9ff70cbddf30e389f31b7f99
Ahmad Ansori Palembani <palembani@gmail.com>
2026-05-13 10:20:04 +0700

    chore: Print the error message

--- generate-cask.rb
+++ generate-cask.rb
@@ -22,6 +22,6 @@ tap.cask_files.each do |path|
   json = JSON.pretty_generate(cask.to_hash_with_variations)
 
   IO.write("_data/cask/#{name}.json", "#{json}\n")
-rescue
-  warn "Error while generating data for '#{path.stem}'"
+rescue => e
+  warn "Error while generating data for '#{path.stem}': #{e.message}"
 end
