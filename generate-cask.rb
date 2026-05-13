62a6cd193009e8d9c7663e9c3452f6dc6ef222f4
Ahmad Ansori Palembani <palembani@gmail.com>
2026-05-13 10:16:25 +0700

    fix: onoe -> warn

--- generate-cask.rb
+++ generate-cask.rb
@@ -17,13 +17,11 @@ tap = Tap.fetch(tap_name)
 Cask::Cask.generating_hash!
 
 tap.cask_files.each do |path|
-  begin
-    cask = Cask::CaskLoader.load(path)
-    name = cask.token
-    json = JSON.pretty_generate(cask.to_hash_with_variations)
+  cask = Cask::CaskLoader.load(path)
+  name = cask.token
+  json = JSON.pretty_generate(cask.to_hash_with_variations)
 
-    IO.write("_data/cask/#{name}.json", "#{json}\n") rescue onoe "Error while generating data for '#{path.stem}'"
-  rescue => e
-    onoe "Something's wrong with '#{path.stem}': #{e.message}"
-  end
+  IO.write("_data/cask/#{name}.json", "#{json}\n")
+rescue
+  warn "Error while generating data for '#{path.stem}'"
 end
